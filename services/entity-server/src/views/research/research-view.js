13d8f57e8ab3e236f50d2aa3c69c6263a3328218
mackeprm <mackeprm@users.noreply.github.com>
2020-03-18 15:14:13 +0100

    * added Text for the Session Visualization * added content for software view

--- services/entity-server/src/views/research/research-view.js
+++ services/entity-server/src/views/research/research-view.js
@@ -1,9 +1,9 @@
-import React, { useRef } from "react";
+import React, {useRef} from "react";
 import propTypes from "prop-types";
 import style from "./research-view.module.css";
 import TwoColumnContent from "../../components/common/two-column-content";
 import PageTitle from "../../components/common/page-title";
-import { useScrollToParagraph } from "../../components/utils";
+import {useScrollToParagraph} from "../../components/utils";
 import ImgWithCaption from "../../components/common/img-with-caption";
 import ideaModelingImg from "../../assets/img/idea-vision-visualization.png";
 import ideaLifecycleImg from "../../assets/img/idea-lifecycle.png";
@@ -27,13 +27,13 @@ export const ResearchView = () => {
   ]);
   return (
     <div className={style.researchViewWrapper}>
-      <PageTitle title={"Research"} />
+      <PageTitle title={"Research"}/>
       <div className={style.researchViewContent}>
-        <div ref={analyzingIdeationRef} />
+        <div ref={analyzingIdeationRef}/>
         <TwoColumnContent
           titleSize={30}
-          right={<div />}
-          left={<AnalyzingIdeationText />}
+          right={<div/>}
+          left={<AnalyzingIdeationText/>}
           title={"Analyzing Ideation"}
         />
         <TwoColumnContent
@@ -45,7 +45,7 @@ export const ResearchView = () => {
               figureNo={1}
             />
           }
-          left={<IdeaModelingText />}
+          left={<IdeaModelingText/>}
           title={"Idea Modeling"}
         />
         <TwoColumnContent
@@ -57,14 +57,14 @@ export const ResearchView = () => {
               figureNo={2}
             />
           }
-          right={<IdeaLifecycleText />}
+          right={<IdeaLifecycleText/>}
           title={"Idea Lifecycle"}
         />
         <h1 ref={understandingIdeationRef} className={style.largeTitle}>
           Understanding Ideation
         </h1>
         <TwoColumnContent
-          left={<IdeaSpaceVisualizationText />}
+          left={<IdeaSpaceVisualizationText/>}
           right={
             <ImgWithCaption
               src={ideaSpaceVisualizationImg}
@@ -82,26 +82,26 @@ export const ResearchView = () => {
               altText="idea-visualization-img"
             />
           }
-          right={<div />}
+          right={<div/>}
           title={"IdeaVisualization"}
         />
         <TwoColumnContent
-          left={<SessionVisualizationText />}
+          left={<SessionVisualizationText/>}
           right={
             <>
-              <img src={sessionVizImg1} alt={"session-view-img"} />
-              <img src={sessionVizImg2} alt={"session-tree-img"} />
+              <img src={sessionVizImg1} alt={"session-view-img"}/>
+              <img src={sessionVizImg2} alt={"session-tree-img"}/>
             </>
           }
           title={"SessionVisualization"}
         />
-        <div ref={augmentingIdeationRef} />
+        <div ref={augmentingIdeationRef}/>
         <TwoColumnContent
           titleSize={30}
           left={
-            <img src={augmentingIdeationImg} alt={"augmenting-ideation-img"} />
+            <img src={augmentingIdeationImg} alt={"augmenting-ideation-img"}/>
           }
-          right={<AugmentingIdeationText />}
+          right={<AugmentingIdeationText/>}
           title={"Augmenting Ideation"}
         />
         <TwoColumnContent
@@ -111,7 +111,7 @@ export const ResearchView = () => {
               alt={"adaptive-ideation-systems-img"}
             />
           }
-          left={<AdaptiveIdeationSystemsText />}
+          left={<AdaptiveIdeationSystemsText/>}
           title={"Adaptive Ideation Systems"}
         />
         <h1 className={style.largeTitle}>Current Projects</h1>
@@ -122,14 +122,14 @@ export const ResearchView = () => {
               alt={"interactive-concept-validation-img"}
             />
           }
-          right={<InteractiveConceptValidationText />}
+          right={<InteractiveConceptValidationText/>}
           title={"Interactive Concept Validation"}
         />
         <TwoColumnContent
           right={
-            <img src={ideaVisualizationImg} alt={"idea-visualization-img"} />
+            <img src={ideaVisualizationImg} alt={"idea-visualization-img"}/>
           }
-          left={<IdeaVisualizationText />}
+          left={<IdeaVisualizationText/>}
           title={"Idea Visualization"}
         />
         <TwoColumnContent
@@ -139,7 +139,7 @@ export const ResearchView = () => {
               alt={"idea-visualization-img"}
             />
           }
-          right={<InspirationRecommenderText />}
+          right={<InspirationRecommenderText/>}
           title={"Inspiration Recommender"}
         />
         <h1 className={style.largeTitle}>Publications</h1>
@@ -188,13 +188,13 @@ PublicationList.propTypes = {
 };
 
 const Publication = props => {
-  const { title, authors, place } = props;
+  const {title, authors, place} = props;
   return (
     <div className={style.publicationWrapper}>
       <b>{authors}</b> <i>{title}</i>
       {place && (
         <>
-          <br />
+          <br/>
           <p>{place}</p>
         </>
       )}
@@ -234,13 +234,15 @@ const IdeaModelingText = () => (
       systems, the insights of Westerski et al. who defined a generalized model
       of ideas and lastly, on our own research in large-scale ideation, where
       ideas are commonly described as short English natural language text
-      snippets. <a href={"/entities/ideas/cf65b021-620f-43fe-9473-1712be788cde"}>Show me an example</a> Furthermore, by defining ideas as
+      snippets. <a href={"/entities/ideas/cf65b021-620f-43fe-9473-1712be788cde"}>Show me an example</a> Furthermore, by
+      defining ideas as
       RDF-Entities, we take into account the dynamic nature of them. RDF as a
       data model allows for the dynamic nature of ideas: Properties and links
       can be added and removed dynamically. In order to reason about ideas, use
       them in an algorithmic system and link them to their context, more
       properties than just the content are needed. To start out we base our idea
-      model on the <a href={"http://www.gi2mo.org/ontology/"} target="_blank" rel="noopener noreferrer">Generic Idea and Innovation Management
+      model on the <a href={"http://www.gi2mo.org/ontology/"} target="_blank" rel="noopener noreferrer">Generic Idea and
+      Innovation Management
       Ontology</a>. This data schema describes ideas
       within an Idea Management System. However, the different aspects of
       ideas seem to be subjective, dynamic and domain-dependent. For example,
@@ -260,11 +262,16 @@ const IdeaModelingText = () => (
 
 const IdeaLifecycleText = () => (
   <p>
-    The second aspects of ideas is that they are living things. Compared to data that lies in tables, an idea can evolve,
-    be further developed, or archived. Furthermore ideas can be split, merged or branched. Ideation is a process, and this
-    process is at least as important then the idea itself. To capture this process, we need to visualize relationships between
-    ideas (both ideas with similar content, or ideas created in the same brainstorming session), and their state of development.
-    Our vision is to have a "github of ideas" system, that enables everybody to iterate, refine or be inspired by each others ideas and
+    The second aspects of ideas is that they are living things. Compared to data that lies in tables, an idea can
+    evolve,
+    be further developed, or archived. Furthermore ideas can be split, merged or branched. Ideation is a process, and
+    this
+    process is at least as important then the idea itself. To capture this process, we need to visualize relationships
+    between
+    ideas (both ideas with similar content, or ideas created in the same brainstorming session), and their state of
+    development.
+    Our vision is to have a "github of ideas" system, that enables everybody to iterate, refine or be inspired by each
+    others ideas and
     captures this process to better understand the process of ideation.
   </p>
 );
@@ -272,26 +279,23 @@ const IdeaLifecycleText = () => (
 const IdeaSpaceVisualizationText = () => (
   <p>
     To understand the opportunity areas of a technology or the topics handled within a challenge, we need to define an
-    <em>Idea Space</em>: A conceptual space of all ideas captured for a challenge together with a spatial relation between
+    <em>Idea Space</em>: A conceptual space of all ideas captured for a challenge together with a spatial relation
+    between
     them. This space helps to understand the output and the thought process within a challenge.
   </p>
 );
 
 const SessionVisualizationText = () => (
   <p>
-    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-    eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-    voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-    clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-    amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-    nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
-    diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-    clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-    amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-    nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed
-    diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-    clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
-    amet.
+    In order to support people in brainstorming tasks, we first need to understand the cognitive processes happening, at
+    least on a heuristic level. Understanding different strategies of how people come up with new ideas based on their
+    surroundings, perception and experiences could lead to detecting and actively encouraging the currently selected
+    problem solving strategy or proactively triggering a strategy change when the current one fails. When an idea is submitted
+    in a context of a brainstorming session, we can track what else happened in this session, on a tracking event level.
+    One way to understand these processes is by visualizing the session data obtained in challenges: For example, by visualizing
+    idea submits and inspiration requests on a <em>Session Timeline</em> we can better understand the impact that
+    inspirations have on subsequent ideas. Furthermore, <em>Session Trees</em> have been used to understand the number of
+    categories and the within category iteraation of ideas for one session.
   </p>
 );
 
