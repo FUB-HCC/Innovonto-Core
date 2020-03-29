38af5c1d652f6fd313a9abb1f9ba92d9e43aa4da
mx-e <maxi.eissler@gmail.com>
2020-03-29 23:11:23 +0200

    IC-47 implemented loading wrapper for centered components with sidebar

--- services/entity-server/src/components/common/page-layouts.js
+++ services/entity-server/src/components/common/page-layouts.js
@@ -5,6 +5,7 @@ import { PageTitle } from "./page-title";
 import { useWindowSize } from "../utils";
 import { footerHeight, headerHeight } from "../../root";
 import { Intent, Spinner } from "@blueprintjs/core";
+import Sidebar from "./sidebar";
 
 export const CenteredLayout = props => {
   const { children, isLoading, error, pageTitle } = props;
@@ -52,4 +53,53 @@ CenteredLayout.propTypes = {
   pageTitle: PropTypes.string
 };
 
-export const FullScreenSideBarLayout = props => {};
+export const FullScreenSideBarLayout = props => {
+  const { children, isLoading, error, pageTitle, sideBarWidth } = props;
+  const [, windowHeight] = useWindowSize();
+  const pageHeight = windowHeight - footerHeight - headerHeight;
+  const LoadedContent = (
+    <div
+      className={style.fullScreenLayoutContent}
+      style={{ minHeight: pageHeight }}
+    >
+      {children}
+    </div>
+  );
+  const LoadingContent = (
+    <div
+      className={style.fullScreenLayoutContent}
+      style={{
+        minHeight: pageHeight
+      }}
+    >
+      <Sidebar
+        width={sideBarWidth}
+        height={pageHeight}
+        title={pageTitle}
+        sideBarModules={[
+          <div />,
+          <Spinner size={60} intent={Intent.SUCCESS} />
+        ]}
+      />
+      <div className={style.loadingMainWindow}>
+        <Spinner size={120} intent={Intent.SUCCESS} />
+      </div>
+    </div>
+  );
+  return (
+    <div
+      className={style.fullScreenLayoutWrapper}
+      style={{ minHeight: pageHeight }}
+    >
+      {isLoading ? LoadingContent : LoadedContent}
+    </div>
+  );
+};
+
+FullScreenSideBarLayout.propTypes = {
+  children: PropTypes.any,
+  isLoading: PropTypes.bool,
+  error: PropTypes.object,
+  pageTitle: PropTypes.string,
+  sideBarWidth: PropTypes.number.isRequired
+};
--- services/entity-server/src/views/solution/solution-view.js
+++ services/entity-server/src/views/solution/solution-view.js
@@ -5,9 +5,11 @@ import SolutionMap from "../../components/solution-map/solution-map";
 import { useWindowSize } from "../../components/utils";
 import { useParams } from "react-router-dom";
 import { footerHeight, headerHeight } from "../../root";
+import { FullScreenSideBarLayout } from "../../components/common/page-layouts";
 
 const solutionHeight = height => height - headerHeight - footerHeight;
 const solutionWidth = width => 1 * width;
+const sideBarWidth = 330;
 
 export const SolutionView = () => {
   let { id } = useParams();
@@ -17,16 +19,19 @@ export const SolutionView = () => {
   const solutionDots = solutionData.bindings ? solutionData.bindings : [];
   const clusterData = solutionData.clusters ? solutionData.clusters : [];
   return (
-    <div className={style.solutionViewWrapper}>
-      <div className={style.solutionViewContent}>
-        <SolutionMap
-          solutionId={id}
-          ideas={solutionDots}
-          clusterData={clusterData}
-          height={solutionHeight(windowHeight)}
-          width={solutionWidth(windowWidth)}
-        />
-      </div>
-    </div>
+    <FullScreenSideBarLayout
+      sideBarWidth={sideBarWidth}
+      isLoading={solutionDots.length < 1}
+      pageTitle={"Idea Map"}
+    >
+      <SolutionMap
+        sideBarWidth={sideBarWidth}
+        solutionId={id}
+        ideas={solutionDots}
+        clusterData={clusterData}
+        height={solutionHeight(windowHeight)}
+        width={solutionWidth(windowWidth)}
+      />
+    </FullScreenSideBarLayout>
   );
 };
