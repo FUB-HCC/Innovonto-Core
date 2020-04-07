19efd97fa7e03c5140a6914b329f2494ea29e376
mx-e <maxi.eissler@gmail.com>
2020-04-07 10:23:00 +0200

    IC did some mobile optimization for the static part of the page, reduced access to the non-mobile optimized part

--- services/entity-server/src/components/common/header.js
+++ services/entity-server/src/components/common/header.js
@@ -6,13 +6,68 @@ import {
   Button,
   NavbarDivider,
   NavbarGroup,
-  NavbarHeading
+  NavbarHeading,
+  Popover,
+  Menu,
+  MenuDivider
 } from "@blueprintjs/core";
+import { useWindowSize } from "../utils";
+import style from "./header.module.css";
 
 const logo = require("../../assets/img/logo-placeholder.png");
 
 export const Header = props => {
   const location = useLocation();
+  const [windowWidth] = useWindowSize();
+  const isOnMobile = windowWidth <= 425;
+  const ResearchLink = (
+    <Link to="/research/-">
+      <Button minimal={true} active={location.pathname.includes("research")}>
+        Research
+      </Button>
+    </Link>
+  );
+
+  const ModelLink = (
+    <Link to="/model">
+      <Button minimal={true} active={location.pathname.includes("model")}>
+        Model
+      </Button>
+    </Link>
+  );
+
+  const DataLink = (
+    <Link to="/data">
+      <Button minimal={true} active={location.pathname.includes("data")}>
+        Data
+      </Button>
+    </Link>
+  );
+
+  const SoftwareLink = (
+    <Link to="/software">
+      <Button minimal={true} active={location.pathname.includes("software")}>
+        Software
+      </Button>
+    </Link>
+  );
+
+  const AboutLink = (
+    <Link to="/about/-">
+      <Button minimal={true} active={location.pathname.includes("about")}>
+        About
+      </Button>
+    </Link>
+  );
+
+  const SearchLink = (
+    <Link to="/search">
+      <Button minimal={true} active={location.pathname.includes("search")}>
+        Search
+      </Button>
+    </Link>
+  );
+
   const { height } = props;
   return (
     <Navbar style={{ height: height }}>
@@ -25,45 +80,48 @@ export const Header = props => {
           </div>
         </NavbarHeading>
       </NavbarGroup>
-      <NavbarGroup align={Alignment.RIGHT}>
-        <Link to="/research/-">
-          <Button
+      {!isOnMobile && (
+        <NavbarGroup align={Alignment.RIGHT}>
+          {ResearchLink}
+          {ModelLink}
+          {DataLink}
+          {SoftwareLink}
+          <NavbarDivider />
+          {AboutLink}
+          {SearchLink}
+        </NavbarGroup>
+      )}
+      {isOnMobile && (
+        <NavbarGroup align={Alignment.RIGHT}>
+          <Popover
+            target={<Button icon={"menu"} />}
             minimal={true}
-            active={location.pathname.includes("research")}
-          >
-            Research
-          </Button>
-        </Link>
-        <Link to="/model">
-          <Button minimal={true} active={location.pathname.includes("model")}>
-            Model
-          </Button>
-        </Link>
-        <Link to="/data">
-          <Button minimal={true} active={location.pathname.includes("data")}>
-            Data
-          </Button>
-        </Link>
-        <Link to="/software">
-          <Button
-            minimal={true}
-            active={location.pathname.includes("software")}
-          >
-            Software
-          </Button>
-        </Link>
-        <NavbarDivider />
-        <Link to="/about/-">
-          <Button minimal={true} active={location.pathname.includes("about")}>
-            About
-          </Button>
-        </Link>
-        <Link to="/search">
-          <Button minimal={true} active={location.pathname.includes("search")}>
-            Search
-          </Button>
-        </Link>
-      </NavbarGroup>
+            content={
+              <Menu>
+                <Link to={"/research/-"}>
+                  <li className={"bp3-menu-item"}>{"Research"}</li>
+                </Link>
+                <Link to={"/model"}>
+                  <li className={"bp3-menu-item"}>{"Model"}</li>
+                </Link>
+                <li className={"bp3-menu-item " + style.disabledMenuItem}>
+                  {"Data  -  Desktop Only"}
+                </li>
+                <Link to={"/software"}>
+                  <li className={"bp3-menu-item"}>{"Software"}</li>
+                </Link>
+                <MenuDivider />
+                <Link to={"/about/-"}>
+                  <li className={"bp3-menu-item"}>{"About"}</li>
+                </Link>
+                <li className={"bp3-menu-item " + style.disabledMenuItem}>
+                  {"Search  -  Desktop Only"}
+                </li>
+              </Menu>
+            }
+          />
+        </NavbarGroup>
+      )}
     </Navbar>
   );
 };
